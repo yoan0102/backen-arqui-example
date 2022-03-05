38872eaebf077dc9828a44a9c466be17591a46e0
Yoan Estrada <yoanestrada01@gmail.com>
2022-03-05 00:47:07 -0500

    ADD: caching implement

--- src/config/index.js
+++ src/config/index.js
@@ -7,4 +7,5 @@ module.exports = {
   MONGO_URI: process.env.MONGO_URI,
   APLICATION_NAME: process.env.APLICATION_NAME,
   JWT_SECRET: process.env.JWT_SECRET,
+  CACHE_KEY: process.env.CACHE_KEY,
 }
